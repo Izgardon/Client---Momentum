638944012ea458b92d6ca38588859f647a9d6529
William <williamsessions23@gmail.com>
2022-06-07 15:49:46 +0100

    added more functionality

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -21,3 +21,42 @@ async function getAllHabits() {
     console.warn(err);
   }
 }
+
+//All moving parts
+
+const dailyBtn = document.querySelector(".new-daily-btn");
+const newDailyLine = document.querySelector(".new-daily-habit-line");
+const newDailySelection = document.querySelector(".new-daily-habit-selection");
+const weeklyBtn = document.querySelector(".new-weekly-btn");
+const newWeeklyLine = document.querySelector(".new-weekly-habit-line");
+const newWeeklySelection = document.querySelector(
+  ".new-weekly-habit-selection"
+);
+
+dailyBtn.addEventListener("click", () => {
+  if (newDailyLine.classList.contains("out-wide-line")) {
+    dailyBtn.classList.remove("spin-reverse");
+    dailyBtn.classList.add("spin");
+    newDailyLine.classList.remove("out-wide-line");
+    newDailySelection.classList.remove("out-wide");
+  } else {
+    dailyBtn.classList.remove("spin");
+    dailyBtn.classList.add("spin-reverse");
+    newDailyLine.classList.add("out-wide-line");
+    newDailySelection.classList.add("out-wide");
+  }
+});
+
+weeklyBtn.addEventListener("click", () => {
+  if (newWeeklyLine.classList.contains("out-wide-line")) {
+    weeklyBtn.classList.remove("spin-reverse");
+    weeklyBtn.classList.add("spin");
+    newWeeklyLine.classList.remove("out-wide-line");
+    newWeeklySelection.classList.remove("out-wide");
+  } else {
+    weeklyBtn.classList.remove("spin");
+    weeklyBtn.classList.add("spin-reverse");
+    newWeeklyLine.classList.add("out-wide-line");
+    newWeeklySelection.classList.add("out-wide");
+  }
+});

ddc582a5d079be7108f23ac096c2b4d7755ad467
William <williamsessions23@gmail.com>
2022-06-08 15:11:16 +0100

    got habits to stay if selected

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,15 +1,43 @@
+//HTML assets
+
+//User profile
+let habitDate = document.querySelector(".date");
+let username = document.querySelector(".username");
+let bestHabit = document.querySelector(".best-habit");
+let worstHabit = document.querySelector(".worst-habit");
+
+//Habits
+let codeHabit = document.querySelector(".code");
+let waterHabit = document.querySelector(".water");
+let outdoorHabit = document.querySelector(".outdoor");
+let projectHabit = document.querySelector(".project");
+let codeChoiceBtn = document.querySelector(".code-choice");
+let waterChoiceBtn = document.querySelector(".water-choice");
+let outdoorChoiceBtn = document.querySelector(".outdoor-choice");
+let projectChoiceBtn = document.querySelector(".project-choice");
+
 //Getting current username of logged in
 
 function currentUser() {
   const username = localStorage.getItem("username");
   return username;
 }
+//Pulling all data of a user
+
+async function getAllData(user) {
+  try {
+    const response = await fetch(`http://localhost:5005/habits/${user}`);
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
+}
 
 //logout of page
 const logoutBtn = document.querySelector("#logout-btn");
-logoutBtn.addEventListener("click", logout, (e) => {
-  console.log("button clicked");
-});
+logoutBtn.addEventListener("click", logout, (e) => {});
 
 function logout() {
   localStorage.clear();
@@ -17,23 +45,93 @@ function logout() {
   location.href = "index.html";
 }
 
-//Pulling all data of a user
+//Loading all data on page load
 
-async function getAllHabits(user) {
-  try {
-    const response = await fetch(`http://localhost:5005/habits/${user}`);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (err) {
-    console.warn(err);
+window.addEventListener("load", async () => {
+  let userData = await getAllData(currentUser());
+  loadProfile(userData[0]);
+  console.log(userData[0]);
+  if (userData[0].habits.code.active == true) {
+    showCode();
   }
+  if (userData[0].habits.water.active == true) {
+    showWater();
+  }
+  if (userData[0].habits.outdoors.active == true) {
+    showOutdoor();
+  }
+  if (userData[0].habits.projects.active == true) {
+    showProject();
+  }
+});
+
+function loadProfile(data) {
+  let date = new Date();
+  username.textContent = data.username;
+  habitDate.textContent = date.toLocaleDateString("en-GB");
 }
 
-window.addEventListener("load", () => {
-  let userData = getAllHabits(currentUser());
-  loadProfile(userData);
+//Adding event listeners to new habit buttons
+
+codeChoiceBtn.addEventListener("click", (e) => {
+  addHabit("code");
+  showCode();
 });
+
+waterChoiceBtn.addEventListener("click", () => {
+  addHabit("water");
+  showWater();
+});
+outdoorChoiceBtn.addEventListener("click", () => {
+  addHabit("outdoors");
+  showOutdoor();
+});
+projectChoiceBtn.addEventListener("click", () => {
+  addHabit("projects");
+  showProject();
+});
+
+function showCode() {
+  codeHabit.classList.remove("hidden");
+  setTimeout(() => {
+    codeHabit.classList.remove("left");
+  }, 1);
+}
+function showWater() {
+  waterHabit.classList.remove("hidden");
+  setTimeout(() => {
+    waterHabit.classList.remove("left");
+  }, 1);
+}
+function showOutdoor() {
+  outdoorHabit.classList.remove("hidden");
+  setTimeout(() => {
+    outdoorHabit.classList.remove("left");
+  }, 1);
+}
+function showProject() {
+  projectHabit.classList.remove("hidden");
+  setTimeout(() => {
+    projectHabit.classList.remove("left");
+  }, 1);
+}
+
+//Adding a habit permanently on screen
+
+async function addHabit(habit) {
+  let user = currentUser();
+  const options = {
+    method: "PATCH",
+    body: JSON.stringify({
+      habit: habit,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+
+  fetch(`http://localhost:5005/habits/${user}`, options);
+}
 
 //All moving parts
 

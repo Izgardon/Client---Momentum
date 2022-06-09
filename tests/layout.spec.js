443c94f3b5715a5449bd120348262a572c568cc0
GMillerMc <gmiller04@yahoo.com>
2022-06-09 17:09:03 +0100

    test event listener

--- tests/layout.spec.js
+++ tests/layout.spec.js
@@ -45,5 +45,46 @@ describe('login form', ()=>{
     })
 })
 
+describe('test event listener', () => {
+    let sut;
+    let events ={};
+    beforeEach(() => {
+        sut = new Dependency();
+        events = {}
+        document.addEventListener = jest.fn((event, callback) => {
+            events[event] = callback;
+    });
+    
+    document.removeEventListener = jest.fn((event, callback) => {
+            delete events[event];
+    });
+    })
+    test("It should pass", () => {
+        const instanceMock = jest.spyOn(sut, "loaded");
+        document.addEventListener = jest
+            .fn()
+            .mockImplementationOnce((event, callback) => {
+            callback();
+            });
+        sut.setupEvents();
+        expect(document.addEventListener).toBeCalledWith(
+            "click",
+            expect.any(Function)
+        );
+        expect(instanceMock).toBeCalledTimes(1);
+    });
+});
+
+class Dependency {
+    setupEvents() {
+        document.addEventListener("click", () =>{
+            this.loaded()
+        });
+    }
+
+    loaded() {
+        console.log('loaded')
+    }
+}
 
 
